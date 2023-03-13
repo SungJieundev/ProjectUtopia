34d757a0f29a99d590168538b392efcd27d17153
alvin0319 <alvin7810@naver.com>
2023-03-14 02:10:04 +0900

    Clear weather when teleporting to other worlds

--- plugins/Weather/src/Loader.php
+++ plugins/Weather/src/Loader.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace alvin0319\Weather;
 
+use alvin0319\Weather\season\Weather;
+use pocketmine\event\entity\EntityTeleportEvent;
+use pocketmine\event\EventPriority;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\SingletonTrait;
 
@@ -15,6 +19,17 @@ final class Loader extends PluginBase{
 	}
 
 	protected function onEnable() : void{
-
+		$this->getServer()->getPluginManager()->registerEvent(EntityTeleportEvent::class, function(EntityTeleportEvent $event) : void{
+			$player = $event->getEntity();
+			if(!$player instanceof Player){
+				return;
+			}
+			$from = $event->getFrom();
+			$to = $event->getTo();
+			if($from->getWorld()->getId() !== $to->getWorld()->getId()){
+				return;
+			}
+			Weather::clearWeather($player);
+		}, EventPriority::NORMAL, $this);
 	}
 }
