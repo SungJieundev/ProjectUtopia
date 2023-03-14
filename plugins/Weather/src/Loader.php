346e899b619d3aff343a56706130fa208da3ea8f
alvin0319 <alvin7810@naver.com>
2023-03-14 14:53:55 +0900

    ...

--- plugins/Weather/src/Loader.php
+++ plugins/Weather/src/Loader.php
@@ -27,7 +27,7 @@ final class Loader extends PluginBase{
 
 	/**
 	 * @var int[]
-	 * @phpstan-param array<int, int> worldId => biomeId
+	 * @phpstan-param array<int, int>
 	 */
 	private static array $worldToBiomeId = [];
 
